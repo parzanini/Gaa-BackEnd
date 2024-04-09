6258d39398a41166779b24f5c981052055c58721
Thiago Gomes Parzanini <parzanini@hotmail.com>
2024-04-10 00:06:06 +0100

    fasd

--- GaaClub/src/main/java/com/gaaclub/dto/AuthenticationDTO.java
+++
@@ -1,7 +0,0 @@
-package com.gaaclub.dto;
-
-import com.gaaclub.model.Role;
-
-public record AuthenticationDTO(String email, String password) {
-
-}
--- GaaClub/src/main/java/com/gaaclub/dto/LoginResponseDTO.java
+++
@@ -1,6 +0,0 @@
-package com.gaaclub.dto;
-
-import com.gaaclub.model.Role;
-
-public record LoginResponseDTO(String token, Role role, String name) {
-}
